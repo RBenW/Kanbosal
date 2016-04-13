27b8e7e110cf29b6ed1c8fe4f6753df3aa034172
RBenW <rbwben@gmail.com>
2016-04-12 19:07:10 -0700

    Insert grants
    ajax call to insert grant, template selection hidden/disabled for now

--- client/javascript/overviewGrants.js
+++ client/javascript/overviewGrants.js
@@ -43,3 +43,59 @@ $(function() {
     html : true
   });
 });
+
+$(function () {
+    $("#cardGenCreate").click(function () {
+		
+        var description = $("#grant-descr-editor").html();
+        var grantName = $("#grantName").val();
+        var grantUrl = $("#grantUrl").val();
+		var cardTemplate = $.ajax({
+			url : '/templates/newCard.html',
+			dataType: 'html',
+			method: 'GET',
+			success: function(data) {
+				newCardTemplate = Handlebars.compile(data);
+			}
+		});
+		
+        var myGrant = {
+            Grant_Name : grantName,
+            Description : description,
+            Url : grantUrl,
+            Users : [],
+            Card_Count : 0,
+            Grant_Columns: [
+                {
+                    Progress: 0.0,
+                    To_Do: [],
+                    In_Progress: [],
+                    Complete: []
+                },
+                {
+                    Progress: 0.0,
+                    To_Do: [],
+                    In_Progress: [],
+                    Complete: []
+                },
+                {
+                    Progress: 0.0,
+                    Cards: []
+                },
+                {
+                    Progress: 0.0,
+                    To_Do: [],
+                    In_Progress: [],
+                    Complete: []
+                }
+            ]
+        };
+		
+        $.ajax({
+            url: 'http://localhost:8080/grants',
+            type: 'PUT',
+            data: JSON.stringify(myGrant),
+            contentType: 'application/json',
+        });
+	});
+});
